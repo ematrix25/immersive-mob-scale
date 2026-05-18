96020b404e01912e0018615355c2af7761d43182
Emanuel <Emanuel@DESKTOP-HP0KVPJ>
2026-05-17 23:17:24 -0300

    chore(init): initialize fabric 26.1.2 mod project. -set up the Fabric example mod environment with Gradle. -validate client execution and entity load events.

---
+++ src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.EntityType;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "modid";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+			if (entity.getType() == EntityType.COW) {
+				LOGGER.info("Cow loaded");
+				LOGGER.info(BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()).toString());
+			}
+		});
+	}
+}
